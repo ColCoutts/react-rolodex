f88f60b16c05ae5069baa17a8a0e850290bf92fb
Colin Coutts <colintcoutts@gmail.com>
2020-11-23 09:47:35 -0800

    styling header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 import './header.styles.css';
 
 const Header = () => (
-  <div className='header'>
-    <p>this will be the header!</p>
-  </div>
+  <header className='header'>
+    <div className='home'>
+      <a href='#landing_page'>&#8493;</a>
+    </div>
+    <nav className='nav menu'>
+      <a href='#about'>about</a>
+      <a href='#work'>work</a>
+      <a href='work'>say hi!</a>
+    </nav>
+  </header>
 );
 
 export default Header;
