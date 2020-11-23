f5e193cd0bc75a82255ad6dde6c903b026e5a288
Colin Coutts <colintcoutts@gmail.com>
2020-11-23 12:18:46 -0800

    build intro section add copy plus styling

--- src/components/introduction/introduction.component.jsx
+++ src/components/introduction/introduction.component.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import './introduction.styles.css';
 
+import introImage from '../../../assets/intro-image.svg';
+
 const Introduction = () => (
   <div className='introduction'>
-    <p>this is my introduction!</p>
+    <div className='intro-image-container'>
+    <img src={introImage} alt='intro image' className='intro-image' />
+    </div>
+    <div className='intro-text'>
+    <p>
+    My name is Colin Coutts, <br />
+    I&apos;m a Software Developer <br />
+    passionate about telling <br />
+    user stories and collaborating <br /> 
+    with other coders to bring<br />
+    projects to life.
+    </p>
+    </div>
   </div>
 );
 
