7a78cff46d15217049ffbec37e0d300cd611e834
Colin Coutts <colintcoutts@gmail.com>
2020-11-27 11:20:35 -0800

    styling project cards

--- src/components/projects/project-item/project-item.component.jsx
+++ src/components/projects/project-item/project-item.component.jsx
@@ -12,18 +12,20 @@ function ProjectItem( { name, tech, image, description, userName, passWord, link
   return (
     <div className='project-item'>
       <div className='image' >
-        <img src={image} alt='work-image' className='work-image'/>
       </div>
-    <h2>{name}</h2>
-    <p>{description}</p>
-    <h3>{userName}</h3>
-    <h3>{passWord}</h3>
-    <i className={tech[0]}></i>
-    <i className={tech[1]}></i>
-    <nav className='nav-links'>
-    <a href={links[0]} target='blank'>info</a>
-    <a href={links[1]} target='blank'>site</a>
-    </nav>
+      <div className='info-container'>
+      <h2 className='project-title'>{name}</h2>
+      <p className='description'>{description}</p>
+      <h3 className='credentials'>{userName}</h3>
+      <h3 className='credentials'>{passWord}</h3>
+      <i className={tech[0]}></i>
+      <i className={tech[1]}></i>
+      <nav className='nav-links'>
+          <a href={links[0]} target='blank'>info</a>
+          <span>|</span>
+      <a href={links[1]} target='blank'>site</a>
+      </nav>
+      </div>
     </div>
     );
 }
