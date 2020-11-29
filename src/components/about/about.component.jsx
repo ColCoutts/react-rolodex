5b469a879ac5a3f75b18a58fd3e8d4eb61a06715
Colin Coutts <colintcoutts@gmail.com>
2020-11-29 13:38:25 -0800

    added react hook to populate static data could be useful if want to fetch this data in future

--- src/components/about/about.component.jsx
+++ src/components/about/about.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './about.styles.css';
 
 import ABOUT_DATA from './about.data';
@@ -6,17 +6,23 @@ import ABOUT_DATA from './about.data';
 import CollectionItem from './collection-item/collection-item.component';
 
 // good time to use React state hooks
-const About = () => (
-  <div className='about'>
-      <div className='collection-list'>
-        {
-          ABOUT_DATA.map( ( { id, ...otherAboutProps } ) => (
-            <CollectionItem key={id} {...otherAboutProps} />
-          ))
-        }
-      </div>
-  </div>
-  );
+const About = () =>
+{ 
+  // eslint-disable-next-line no-unused-vars
+  const [data, setData] = useState( ABOUT_DATA );
 
+  return(
+    <div className='about'>
+    <div className='collection-list'>
+    {
+      data.map( ( { id, ...otherAboutProps } ) => (
+        <CollectionItem key={id} {...otherAboutProps} />
+        ))
+      }
+      </div>
+      </div>
+      );
+      
+    };
 
 export default About;
